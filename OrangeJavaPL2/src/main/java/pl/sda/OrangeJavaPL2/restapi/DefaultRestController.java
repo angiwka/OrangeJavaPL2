5db03d89484063f43dcffc12b340dabe2ba4937c
Lenovogit1 <gilewskanna@gmail.com>
2023-04-22 10:05:55 +0200

    add DefaultRestController

---
+++ OrangeJavaPL2/src/main/java/pl/sda/OrangeJavaPL2/restapi/DefaultRestController.java
@@ -0,0 +1,15 @@
+package pl.sda.OrangeJavaPL2.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //bean definition
+public class DefaultRestController {
+    @GetMapping("/hello")
+    // @RequestMapping(method= RequestMethod.GET, path = "/hello") //Both mappings do the same job
+    public String hello() {
+        return "Hello ByczQ from anywhere";
+    }
+}
