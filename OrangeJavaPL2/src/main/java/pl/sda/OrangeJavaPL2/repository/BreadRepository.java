69addcbba162f8e8092727eef42445894844ec09
Lenovogit1 <gilewskanna@gmail.com>
2023-04-22 11:23:03 +0200

    add initial data to database

---
+++ OrangeJavaPL2/src/main/java/pl/sda/OrangeJavaPL2/repository/BreadRepository.java
@@ -0,0 +1,22 @@
+package pl.sda.OrangeJavaPL2.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import pl.sda.OrangeJavaPL2.entity.Bread;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BreadRepository {
+
+    private final IBreadRepository breadRepository;
+
+    List<Bread> getAllBreads() {
+        return breadRepository.findAll();
+    }
+
+    public void addBread(Bread bread){
+        breadRepository.save(bread);
+    }
+}
